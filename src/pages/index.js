8ddb5e26405b66b4234572d225d990a77a6c3ce0
jgbkid <mvenice66@gmail.com>
2018-09-05 21:47:41 -0700

    tips that helped me get the repo working

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,8 @@ const IndexPage = () => (
     <h1>Party people yeah</h1>
     <p>Welcome to your new Gatsby site. It's the shit.</p>
     <p>Now go build something great.</p>
-    <p>When you add a github repo, make sure you select https and not ssh.</p>
+    <p>When you add a github repo, make sure you select https and not ssh. Also to add files that weren't originally staged, use 'git status' and then 'git add .' to add all. If you get fatal 'origin already exists' here are some important commands— 'git remote rm origin' and 'git remote rm upstream'.
+    </p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
