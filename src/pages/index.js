e3c534d9d4e0d6f9f024e41a0002fba6cdc011c9
jgbkid <mvenice66@gmail.com>
2018-09-05 21:40:06 -0700

    added a new line

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ const IndexPage = () => (
     <h1>Party people yeah</h1>
     <p>Welcome to your new Gatsby site. It's the shit.</p>
     <p>Now go build something great.</p>
+    <p>When you add a github repo, make sure you select https and not ssh.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
