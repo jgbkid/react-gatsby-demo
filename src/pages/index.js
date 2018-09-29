e42ccac952e77ebc69b056742073130a96fd546f
jgbkid <mvenice66@gmail.com>
2018-09-29 09:19:19 -0700

    using static data, json file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,33 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import Card from '../components/Card';
+import Card from '../components/card';
+import Section from '../components/section';
+import Wave from '../components/wave';
+import staticdata from '../../staticdata.json';
+import Cell from '../components/cell';
+import styled from 'styled-components';
 
+const SectionCaption = styled.p`
+    font-weight:600;
+    font-size: 18px;
+    text-transform:uppercase;
+    color: #94a4ba;
+    text-align: center;
+`
+
+const SectionCellGroup = styled.div`
+    max-width:800px;
+    margin: 0 auto 100px;
+    display:grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-column-gap: 20px;
+    padding: 0 20px;
+
+    @media (max-width: 800px) {
+      grid-template-columns: repeat(1, 1fr);
+     
+    }
+`
 
 const IndexPage = () => (
   <div>
@@ -18,23 +44,18 @@ const IndexPage = () => (
         <img src={require('../images/logo-framer.png')} width="50"/>
         <img src={require('../images/logo-react.png')} width="50"/>
         <img src={require('../images/logo-swift.png')} width="50"/>
+        {/* <img src={require('https://www.dropbox.com/s/syvrx3yaxyh1bq9/logo-react.png?dl=1')} width="50"/> */}
+        {/* <img src="https://www.dropbox.com/s/syvrx3yaxyh1bq9/logo-react.png?dl=1" width="50"/> */}
         </div>
 
 
-        <svg width="100%" height="172" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="" fill="white">
-        <animate repeatCount="indefinite" fill="freeze" attributeName="d" dur="10s" values="
-        M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;
-
-        M0 87.1596C316 87.1597 444 160 884 52.0001C1324 -55.9999 1320.29 34.966 1538 71.251C1814 117.251 2156 189.252 2560 87.1597V233.161L0 233.161V87.1596Z;
-
-        M0 53.6584C158 11.0001 213 0 363 0C513 0 855.555 115.001 1154 115.001C1440 115.001 1626 -38.0004 2560 53.6585V199.66L0 199.66V53.6584Z
-        "/>
-
-        </path>
-        </svg>
+        <Wave/>
       </div>   
     </div>
+
+
+  
+    
     <div className="Cards">
       <h2>11 courses, more coming</h2>
         <div className="CardGroup">
@@ -58,8 +79,24 @@ const IndexPage = () => (
             text="12 sections"
             image={require('../images/wallpaper4.jpg')}
           />
+
           </div>
       </div>
+      <Section 
+        image={require('../images/wallpaper2.jpg')}
+        logo={require('../images/logo-react.png')}
+        title="React for Designers"
+        text="Learn how to build a modern site using React and the most efficient libraries to get your site/product online. Get familiar with Grid CSS, animations, interactions, dynamic data with Contentful and deploying your site with Netlify."
+      />
+     
+      <SectionCaption>12 sections - 6 hours</SectionCaption>
+      <SectionCellGroup>
+      {staticdata.cells.map(cell => (
+        <Cell 
+          title={cell.title} 
+          image={cell.image} />
+      ))}
+      </SectionCellGroup>
   </div>
 )
 
